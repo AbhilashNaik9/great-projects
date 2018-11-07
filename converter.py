a10d533654765efac0d6ef82a7c5ddb2f9dc6ba4
AbhilashNaik9 <44835972+AbhilashNaik9@users.noreply.github.com>
2018-11-07 16:33:32 +0530

    change

---
+++ converter.py
@@ -0,0 +1,9 @@
+# temperature converter
+
+def main():
+    celsius = eval(input("what is the celesius temperature ?"))
+    fahrenheit = 9/5 * celsius + 32
+    print("the temperature is", fahrenheit, "degrees fahrenheit")
+    
+main()
+
